db1212ede9604703c955577dfb5e3dae8846cf61
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-15 20:41:55 +0100

    Deploy

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-
 const app = express();
-app.use(bodyParser.json());
-
-app.post('/', (req, res) => {
-  const email = req.body.email;
-  if (!email) {
-    res.status(400).send('Error: No email provided.');
-  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
-    res.status(400).send('Error: Invalid email format.');
-  } else {
-    res.send(email);
-  }
-});
-
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
