6e3b3fe9d66ba54d884e8618a392dd08c5557ffc
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-15 21:10:55 +0100

    Deploy

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from 'express';
 const app = express();
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  const email = req.body.email;
+  if (!email) {
+    return res.status(400).json({ error: 'No email provided' });
+  }
+  const emailParts = email.split('@');
+  const username = emailParts[0];
+  res.json({ username });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
