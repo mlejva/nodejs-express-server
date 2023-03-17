0582b5d46ce7d2c1ca48137e88a38a1d6a44f80b
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-17 14:58:23 +0100

    Deploy

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 
+            import { createRequire } from "module";
+            const require = createRequire(import.meta.url);
+            const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
   const name = req.body.name;
   if (!name) {
-    res.status(400).send('Error: No name specified');
-  } else {
-    const names = name.split(' ');
-    const firstName = names[0];
-    const lastName = names.length > 1 ? names[names.length - 1] : '';
-    res.send({ firstName, lastName });
+    return res.status(400).json({ error: 'Name is required' });
   }
+  const names = name.split(' ');
+  const firstName = names[0];
+  const lastName = names.length > 1 ? names[names.length - 1] : '';
+  return res.json({ firstName, lastName });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
