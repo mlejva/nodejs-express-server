d5fdea89c23337928be37cf41dba62f053e466af
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-18 19:12:04 +0100

    deploy

--- index.js
+++ index.js
@@ -3,30 +3,31 @@
             const require = createRequire(import.meta.url);
             
 import express from 'express';
+import { WebClient } from '@slack/web-api';
+
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
-app.post('/', (req, res) => {
-  const email = req.body.email;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    res.status(400).send('Invalid email format');
-  } else {
-    const SlackREST = require('@sagi.io/workers-slack');
-    const botAccessToken = 'xoxb-625989678439-4949424452337-fLgQoJFqYBYyzOeoRfZ1WMrj';
-    const SlackAPI = new SlackREST({ botAccessToken });
-    const data = { channel: 'general', text: `User '${email}' signed up` };
-    SlackAPI.chat.postMessage(data)
-      .then(() => {
-        res.send('Success');
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error sending message to Slack');
-      });
+app.post('/', async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).send('Email is required');
   }
+
+  const slackClient = new WebClient(process.env.SLACK_ACCESS_TOKEN);
+  const message = `User '${email}' signed up`;
+  try {
+    await slackClient.chat.postMessage({
+      channel: 'general',
+      text: message,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.send('Success');
 });
 
 app.listen(port, () => {
