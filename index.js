16d54abb886cb192d3277c835c963d69fb1098f9
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2023-03-18 20:13:52 +0100

    Deploy

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 
             import { createRequire } from "module";
             const require = createRequire(import.meta.url);
-            
-import express from 'express';
-import { WebClient } from '@slack/web-api';
-
-const app = express();
-const port = 3000;
-
-app.use(express.json())
-
-app.post('/', (req, res) => {
-  const { email } = req.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!email || !emailRegex.test(email)) {
-    return res.status(400).send('Invalid email');
-  }
-
-  const slackClient = new WebClient(process.env.SLACK_ACCESS_TOKEN);
-  const message = `User '${email}' signed up`;
-
-  slackClient.chat.postMessage({
-    channel: 'general',
-    text: message,
-  });
-
-  return res.status(200).send('Success');
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+            $ACTION_OUTPUT
